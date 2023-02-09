23ab1cbf408507c84bb068857d4e85c2b6317916
kimhamel <124894314+kimhamel@users.noreply.github.com>
2023-02-09 22:52:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
+from discord_buttons_plugin import *
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -10,10 +11,31 @@ TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
 
+buttons = ButtonsClient(client)
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
 
+@bot.command()
+async def 버튼(ctx):
+    content = "호두 사진을 보낼것이라면 버튼을 눌러주세요.",
+    channel = ctx.channel.id,
+    components = [
+        ActionRow([
+            Button(
+                label="권한",
+                style=ButtonType().Primary,
+                custom_id="button_one"
+            )
+        ])
+    ]
+)
+    
+@buttons.click
+async def button_one(ctx):
+    await ctx.reply("버튼이 눌렸습니다.")
+    
 @client.event
 async def on_message(message):
     await message.add_reaction("🇭")
